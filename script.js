42b9885d9a9e2d7cf1bed06eb2b08b9549442455
laimuIsdude <zicheng213@gmail.com>
2026-01-04 18:01:43 -0700

    loadpartial js to fix

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+const loadPartials = (className, file) => {
+    const target = document.querySelectorAll(className);
+    if (!target) return;
+    
+    fetch(file)
+        .then(res => res.text())
+        .then(data => {
+            target.forEach(el => {
+                el.innerHTML = data;
+            });
+        })
+}
+
+loadPartials('.herocard-container', 'partials/aside.html');
