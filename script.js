89cc4e877ad34e868431301e0b74ed59192a26a1
laimuIsdude <zicheng213@gmail.com>
2026-01-06 23:07:32 -0700

    JS in progress - insertAdjacentHTML

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const loadPartials = (className, file) => {
-    const target = document.querySelectorAll(className);
-    if (!target) return;
-    
-    fetch(file)
-        .then(res => res.text())
-        .then(data => {
-            target.forEach(el => {
-                el.innerHTML = data;
-            });
-        })
-}
+const loadPartials = (selector, file) => {
+  const targets = document.querySelectorAll(selector);
+  if (targets.length === 0) return;
+
+  fetch(file)
+    .then(res => res.text())
+    .then(html => {
+      targets.forEach(el => {
+        el.insertAdjacentHTML('beforeend', html);
+      });
+    })
+    .catch(err => console.error(err));
+};
 
 loadPartials('.herocard-container', 'partials/aside.html');
